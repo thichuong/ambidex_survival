1c16759f160e0ebfd79aa975a8cb110947b20305
Thi Thanh Chuong <55147306+thichuong@users.noreply.github.com>
2025-12-19 20:39:38 +0700

    refactor: simplify game states and extract projectile collision and hit logic into helper functions in combat system.

--- src/resources/game_state.rs
+++ src/resources/game_state.rs
@@ -3,10 +3,7 @@ use bevy::prelude::*;
 #[derive(States, Debug, Clone, Copy, Eq, PartialEq, Hash, Default)]
 
 pub enum GameState {
-    AssetLoading,
-    Menu,
     Playing,
-    Shop,
     #[default]
     WeaponMenu,
     GameOver,
