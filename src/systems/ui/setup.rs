b4798183bb6ec62b022be92a68347ee577440152
Thi Thanh Chuong <55147306+thichuong@users.noreply.github.com>
2025-12-21 20:59:30 +0700

    chore: Add clippy allow attribute for needless_pass_by_value to `setup_ui` function.

--- src/systems/ui/setup.rs
+++ src/systems/ui/setup.rs
@@ -3,6 +3,7 @@ use super::hud::spawn_hud;
 use super::menu::spawn_weapon_menu;
 use bevy::prelude::*;
 
+#[allow(clippy::needless_pass_by_value)]
 pub fn setup_ui(mut commands: Commands, asset_server: Res<AssetServer>) {
     spawn_hud(&mut commands, &asset_server);
     spawn_weapon_menu(&mut commands);
