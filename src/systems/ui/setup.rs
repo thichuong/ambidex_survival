42d925feb2130379d8e999afe958bb11b6d4d239
Thi Thanh Chuong <55147306+thichuong@users.noreply.github.com>
2025-12-21 16:45:40 +0700

    refactor: remove clippy allow attributes from UI setup

--- src/systems/ui/setup.rs
+++ src/systems/ui/setup.rs
@@ -3,7 +3,6 @@ use super::hud::spawn_hud;
 use super::menu::spawn_weapon_menu;
 use bevy::prelude::*;
 
-#[allow(clippy::too_many_lines, clippy::needless_pass_by_value)]
 pub fn setup_ui(mut commands: Commands, asset_server: Res<AssetServer>) {
     spawn_hud(&mut commands, &asset_server);
     spawn_weapon_menu(&mut commands);
