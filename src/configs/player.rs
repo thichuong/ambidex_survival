ad32b03624e301288c2190c4e1cd443e1934634e
Thi Thanh Chuong <55147306+thichuong@users.noreply.github.com>
2025-12-22 21:14:09 +0700

    fix: reduce player starting gold from 1500 to 300

--- src/configs/player.rs
+++ src/configs/player.rs
@@ -2,4 +2,4 @@ use bevy::prelude::*;
 
 pub const RADIUS: f32 = 20.0;
 pub const COLOR: Color = Color::srgb(0.0, 1.0, 1.0); // AQUA
-pub const STARTING_GOLD: u32 = 1500;
+pub const STARTING_GOLD: u32 = 300;
